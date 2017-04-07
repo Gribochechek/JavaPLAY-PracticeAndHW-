7c88081a8152382c98e51dd3735f200190e9c4f1
Roman.Sakh <gribochechek@gmail.com>
2017-04-07 16:23:40 +0300

    22

--- src/sakhroman/lection20Practice/Car.java
+++ src/sakhroman/lection20Practice/Car.java
@@ -4,5 +4,18 @@ abstract class Car {
 
 	private String modelName;
 	private String makeName;
+	private boolean isClean = false;
+	private double tirePress;
+	
+	public double normilizePressure(double tirePress){
+		
+		return tirePress;
+		
+	}
+
+	public void setTirePress(double tirePress) {
+		this.tirePress = tirePress;
+	}
+	
 	
 }
